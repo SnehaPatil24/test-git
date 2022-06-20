f5d3a48bb8233bf585bb474a62216c06f784ba46
Sneha <snehapatil2421@gmail.com>
2022-06-20 22:41:08 +0530

    space

--- First project/src/abc/Mock_program1.java
+++ First project/src/abc/Mock_program1.java
@@ -6,7 +6,9 @@ public class Mock_program1 {
 	  public static void main(String[] args) {
 		  Mock_program1 obj=new Mock_program1();
 		  obj.add(12, b);
-		 float x= Mock_program1.add(12.3f, 43);
+
+		  
+		  float x= Mock_program1.add(12.3f, 43);
 		 System.out.println(x);
 		  int p= Mock_program1.add(24, 54);
 		  System.out.println(p);
